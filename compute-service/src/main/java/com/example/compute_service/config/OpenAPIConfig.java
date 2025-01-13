ff6e1e3097643fd7926942862afc894e0c329739
jpzambrano <jpzambrano@gmail.com>
2025-01-13 17:50:05 -0300

    Update dependencies in pom.xml, enhance OpenAPI configuration, and refine application.yml logging settings

--- compute-service/src/main/java/com/example/compute_service/config/OpenAPIConfig.java
+++ compute-service/src/main/java/com/example/compute_service/config/OpenAPIConfig.java
@@ -10,16 +10,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenAPIConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Compute Service API")
-                        .description("API for dynamic percentage calculations and history management")
-                        .version("1.0.0")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("API Documentation")
-                        .url("https://example.com/docs"));
-    }
+  
+        @Bean
+        public OpenAPI customOpenAPI() {
+            return new OpenAPI()
+                    .openapi("3.0.0")  // Aquí especificas la versión correcta
+                    .info(new Info()
+                            .title("Compute Service API")
+                            .description("API for dynamic percentage calculations and history management")
+                            .version("1.0.0")
+                            .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                    .externalDocs(new ExternalDocumentation()
+                            .description("API Documentation")
+                            .url("https://example.com/docs"));
+        }
 }
