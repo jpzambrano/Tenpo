16e293c899bc2b88f88c35352d6cfaf5a4686d2d
jpzambrano <jpzambrano@gmail.com>
2025-01-13 19:54:31 -0300

    Enable asynchronous processing, add validation to CalculationRequest fields, and adjust HistoryService to save timestamps with system timezone

--- compute-service/src/main/java/com/example/compute_service/model/CalculationRequest.java
+++ compute-service/src/main/java/com/example/compute_service/model/CalculationRequest.java
@@ -1,7 +1,9 @@
 package com.example.compute_service.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Request payload for performing a calculation")
-public record CalculationRequest( @Schema(description = "First number", example = "10") double num1,
-        @Schema(description = "Second number", example = "20") double num2) {}
+public record CalculationRequest( @Schema(description = "First number", example = "10") @NotNull(message = "num1 cannot be null") double num1,
+        @Schema(description = "Second number", example = "20") @NotNull(message = "num2 cannot be null") double num2) 
+        {}
--- compute-service/src/main/java/com/example/compute_service/service/HistoryService.java
+++ compute-service/src/main/java/com/example/compute_service/service/HistoryService.java
@@ -5,10 +5,13 @@ import com.example.compute_service.repository.HistoryRepository;
 
 import reactor.core.publisher.Mono;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,13 +22,13 @@ public class HistoryService {
     public HistoryService(HistoryRepository historyRepository) {
         this.historyRepository = historyRepository;
     }
-
+ @Async
     public void saveCalculation(double num1, double num2, double result) {
         CalculationHistory history = new CalculationHistory();
         history.setNum1(num1);
         history.setNum2(num2);
         history.setResult(result);
-        history.setTimestamp(LocalDateTime.now());
+        history.setTimestamp(LocalDateTime.now(ZoneId.systemDefault())); // Ajustado a LocalDateTime
         historyRepository.save(history);
     }
 
